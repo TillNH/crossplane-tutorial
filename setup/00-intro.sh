01bab1ec2bea23fd37361fba378810f183a73f3c
Till Huslik <till.huslik@accenture.com>
2025-07-17 17:48:31 +0200

    Intro

--- setup/00-intro.sh
+++ setup/00-intro.sh
@@ -12,65 +12,65 @@ Select "Yes" only if you did NOT follow the story from the start (if you jumped 
 Feel free to say "No" and inspect the script if you prefer setting up resources manually.
 ' || exit 0
 
-echo "
-## You will need following tools installed:
-|Name            |Required             |More info                                          |
-|----------------|---------------------|---------------------------------------------------|
-|Linux Shell     |Yes                  |Use WSL if you are running Windows                 |
-|Docker          |Yes                  |'https://docs.docker.com/engine/install'           |
-|kind CLI        |Yes                  |'https://kind.sigs.k8s.io/docs/user/quick-start/#installation'|
-|kubectl CLI     |Yes                  |'https://kubernetes.io/docs/tasks/tools/#kubectl'  |
-|crossplane CLI  |Yes                  |'https://docs.crossplane.io/latest/cli'            |
-|yq CLI          |Yes                  |'https://github.com/mikefarah/yq#install'          |
-|Google Cloud account with admin permissions|If using Google Cloud|'https://cloud.google.com'|
-|Google Cloud CLI|If using Google Cloud|'https://cloud.google.com/sdk/docs/install'        |
-|AWS account with admin permissions|If using AWS|'https://aws.amazon.com'                  |
-|AWS CLI         |If using AWS         |'https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html'|
-|Azure account with admin permissions|If using Azure|'https://azure.microsoft.com'         |
-|az CLI          |If using Azure       |'https://learn.microsoft.com/cli/azure/install-azure-cli'|
+# echo "
+# ## You will need following tools installed:
+# |Name            |Required             |More info                                          |
+# |----------------|---------------------|---------------------------------------------------|
+# |Linux Shell     |Yes                  |Use WSL if you are running Windows                 |
+# |Docker          |Yes                  |'https://docs.docker.com/engine/install'           |
+# |kind CLI        |Yes                  |'https://kind.sigs.k8s.io/docs/user/quick-start/#installation'|
+# |kubectl CLI     |Yes                  |'https://kubernetes.io/docs/tasks/tools/#kubectl'  |
+# |crossplane CLI  |Yes                  |'https://docs.crossplane.io/latest/cli'            |
+# |yq CLI          |Yes                  |'https://github.com/mikefarah/yq#install'          |
+# |Google Cloud account with admin permissions|If using Google Cloud|'https://cloud.google.com'|
+# |Google Cloud CLI|If using Google Cloud|'https://cloud.google.com/sdk/docs/install'        |
+# |AWS account with admin permissions|If using AWS|'https://aws.amazon.com'                  |
+# |AWS CLI         |If using AWS         |'https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html'|
+# |Azure account with admin permissions|If using Azure|'https://azure.microsoft.com'         |
+# |az CLI          |If using Azure       |'https://learn.microsoft.com/cli/azure/install-azure-cli'|
 
-If you are running this script from **Nix shell**, most of the requirements are already set with the exception of **Docker** and the **hyperscaler account**.
-" | gum format
+# If you are running this script from **Nix shell**, most of the requirements are already set with the exception of **Docker** and the **hyperscaler account**.
+# " | gum format
 
-gum confirm "
-Do you have those tools installed?
-" || exit 0
+# gum confirm "
+# Do you have those tools installed?
+# " || exit 0
 
-rm -f .env
+# rm -f .env
 
-#########################
-# Control Plane Cluster #
-#########################
+# #########################
+# # Control Plane Cluster #
+# #########################
 
-kind create cluster --config kind.yaml
+# kind create cluster --config kind.yaml
 
-kubectl apply \
-    --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+# kubectl apply \
+#     --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
-##############
-# Crossplane #
-##############
+# ##############
+# # Crossplane #
+# ##############
 
-helm upgrade --install crossplane crossplane \
-    --repo https://charts.crossplane.io/stable \
-    --namespace crossplane-system --create-namespace --wait
+# helm upgrade --install crossplane crossplane \
+#     --repo https://charts.crossplane.io/stable \
+#     --namespace crossplane-system --create-namespace --wait
 
-kubectl apply \
-    --filename providers/provider-kubernetes-incluster.yaml
+# kubectl apply \
+#     --filename providers/provider-kubernetes-incluster.yaml
 
-kubectl apply --filename providers/provider-helm-incluster.yaml
+# kubectl apply --filename providers/provider-helm-incluster.yaml
 
-kubectl apply --filename providers/dot-kubernetes.yaml
+# kubectl apply --filename providers/dot-kubernetes.yaml
 
-kubectl apply --filename providers/dot-sql.yaml
+# kubectl apply --filename providers/dot-sql.yaml
 
-kubectl apply --filename providers/dot-app.yaml
+# kubectl apply --filename providers/dot-app.yaml
 
-gum spin --spinner dot \
-    --title "Waiting for Crossplane providers..." -- sleep 60
+# gum spin --spinner dot \
+#     --title "Waiting for Crossplane providers..." -- sleep 60
 
-kubectl wait --for=condition=healthy provider.pkg.crossplane.io \
-    --all --timeout=1800s
+# kubectl wait --for=condition=healthy provider.pkg.crossplane.io \
+#     --all --timeout=1800s
 
 echo "## Which Hyperscaler do you want to use?" | gum format
 
